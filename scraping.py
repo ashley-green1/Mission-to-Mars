5b6c9bec12d032b57b7c818018ceed2f6c480600
Ashley Green <75928657+ashley-green1@users.noreply.github.com>
2021-03-30 02:22:37 -0500

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -28,6 +28,33 @@ def mars_news(browser):
         
     return news_title, news_p
 
+def featured_image(browser):
+
+    # Visit URL
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+    # find the relative image url
+    img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    
+    return img_url
+
 
 # End the automated browsing session
 # browser.quit()
